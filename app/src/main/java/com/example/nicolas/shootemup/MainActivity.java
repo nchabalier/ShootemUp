b1ae2a4f78bfdc8123538489cc913877bb9d5bfb
nichabalie <nichabalie@poste.isima.fr>
2016-12-11 22:54:41 -0500

    Add name field and automaticly at the name in scoreboard when we start playing

--- app/src/main/java/com/example/nicolas/shootemup/MainActivity.java
+++ app/src/main/java/com/example/nicolas/shootemup/MainActivity.java
@@ -6,18 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private ImageButton buttonOptions;
     private ImageButton buttonUpdgrade;
     private Button buttonScore;
+    private EditText editPseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editPseudo = (EditText) findViewById(R.id.editPseudo);
 
         buttonOptions = (ImageButton) findViewById(R.id.buttonOptions);
         buttonOptions.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +38,16 @@ public class MainActivity extends Activity {
         buttonUpdgrade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String name = editPseudo.getText().toString();
+
+                //If the current name is not in the data base we add it with a score equal to 0
+                ScoreBoardDAO scoreBoardDAO = new ScoreBoardDAO(getBaseContext());
+                scoreBoardDAO.open();
+                scoreBoardDAO.add(new ScoreBoard(1, name, 0));
+                scoreBoardDAO.close();
+
+                //TODO: passer le "name" en extra dans l'intent
                 Intent upgradeActivityIntent = new Intent(MainActivity.this, UpgradeActivity.class);
                 startActivity(upgradeActivityIntent);
             }
